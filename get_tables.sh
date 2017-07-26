009ac8a0b89ccaa24dafc951ae83d109ef29536e
Damien Saucez <damien.saucez@inria.fr>
2017-07-26 14:56:29 +0200

    tidy the repository

--- get_tables.sh
+++
@@ -1,3 +0,0 @@
-DIR=$1
-ssh 10.0.2.2 -o port=41011 "sudo  ovs-ofctl -O OpenFlow13 dump-flows br-int" > $DIR>ovs1.table
-ssh 10.0.2.2 -o port=41012 "sudo  ovs-ofctl -O OpenFlow13 dump-flows br-int" > $DIR>ovs2.table
